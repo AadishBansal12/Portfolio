260b18f9f26af450c39321ef4a13385a676605a8
Aadish <500094812@stu.upes.ac.in>
2023-11-30 00:29:54 +0530

    Update send-mail.php

--- send-mail.php
+++ send-mail.php
@@ -1,50 +1,38 @@
 <?php
 
-	// site owner
-	$to = trim( $_POST['to'] );
-	$subject = trim( $_POST['subject'] );
+// site owner
+$to = 'aadishbansal12@gmail.com';
+$subject = trim($_POST['subject']);
 
-	// contact form fields
-	$name = trim( $_POST['name'] );
-	$email = trim( $_POST['email'] );
-	$message = trim( $_POST['message'] );
-	
+// contact form fields
+$name = trim($_POST['name']);
+$email = trim($_POST['email']);
+$message = trim($_POST['message']);
 
-	// check for error
-	$error = false;
-	
-	if ( $name === "" )
-	{
-		$error = true;
-	}
-	elseif ( $email === "" )
-	{
-		$error = true;
-	}
-	elseif ( $message === "" )
-	{
-		$error = true;
-	}
-	// end check for error
-	
-	// no error send mail
-	if ( !$error )
-	{
-		
-		$body = "Name: $name \n\nEmail: $email \n\nMessage: $message";
-		
-		$headers = 'From: ' . $name . ' <' . $email . '> ' . "\r\n" . 'Reply-To: ' . $email;
-		
-		mail( $to, $subject, $body, $headers );
-		
-		echo 'success';
-		
-	}
-	else
-	{
-		echo 'error';
-	}
-	// end no error send mail
+// check for error
+$error = false;
 
-	
+if ($name === "") {
+    $error = true;
+} elseif ($email === "") {
+    $error = true;
+} elseif ($message === "") {
+    $error = true;
+}
+// end check for error
+
+// no error send mail
+if (!$error) {
+
+    $body = "Name: $name \n\nEmail: $email \n\nMessage: $message";
+
+    $headers = 'From: ' . $name . ' <' . $email . '> ' . "\r\n" . 'Reply-To: ' . $email;
+
+    mail($to, $subject, $body, $headers);
+
+    echo 'success';
+} else {
+    echo 'error';
+}
+// end no error send mail
 ?>
